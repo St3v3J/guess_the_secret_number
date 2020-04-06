541b0d86403b4dbac2b5cb3f815acf478956c1ce
Steve <stevendgjohnson@gmail.com>
2020-04-06 15:23:08 -0500

    guess the secret number

---
+++ index.py
@@ -0,0 +1,8 @@
+secret = 11
+
+guess = int(input("Guess the secret number please (between 1 and 30): "))
+
+if guess == secret:
+    print("Excellent, your a Jedi Ninja! It is the number 11!")
+else:
+    print("Wrong, like wow your way off!1")
